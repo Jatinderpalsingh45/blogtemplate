5a4d091694b6b4943d182ce60f7186b318af71e4
Jatinderpal Singh <jatinderaulakh4545@gmail.com>
2021-05-13 13:28:42 -0400

    files uploaded

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 24,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
